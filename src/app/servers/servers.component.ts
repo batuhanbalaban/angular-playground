bdb09588ab82fc3bade959d79aab247bedef1c66
unknown <batuhan.balaban@yahoo.com>
2019-10-02 17:13:12 +0300

    ngIf  else built-in Directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -17,6 +17,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = '';
+  serverCreated = false;
 
   constructor() {
     setTimeout(()=>{
@@ -28,7 +29,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
-    this.serverCreationStatus = 'Server was created.'
+    this.serverCreated = true;
   }
   onUpdateServerName(event : Event){
     this.serverName = (<HTMLInputElement>event.target).value;
