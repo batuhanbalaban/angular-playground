457d0f44862b387b69100b847b32f4f8f10828a4
unknown <batuhan.balaban@yahoo.com>
2019-10-02 15:39:54 +0300

    selector also working with attribute or class

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector with htmltag
+  // selector: 'app-servers',
+
+  // selector with Attribute
+  // selector: '[app-servers]',
+
+  // selector with Class
+  selector: '.app-servers',
+
   template: `
   <app-server></app-server> 
   <app-server></app-server>
